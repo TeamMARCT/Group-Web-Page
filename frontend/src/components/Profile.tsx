4e2dfff74bf1161a5f101f739d61e2db7ba13032
cmloof <cmlooff@gmail.com>
2022-09-28 10:31:34 -0400

    Adding social links for profiles

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -2,20 +2,34 @@ import React from 'react'
 import { Card } from 'react-bootstrap'
 
 // Makes it clear that this is a Functional Component
-const Profile: React.FC<{profile: {name: string, image: string, description: string}}> = ({profile}) => {
+const Profile: React.FC<{profile: {name: string, image: string, description: string, linkedin: string, github: string}}> = ({profile}) => {
   return (
     <Card className='my-3 p-3 rounded'>
-      <a href={`/`}>
+      <a href={`${profile.linkedin}`}>
         <Card.Img src={profile.image} variant='top'/>
       </a>
 
       <Card.Body>
-      <a href={`/`}>
+      <a href={`${profile.linkedin}`}>
         <Card.Title as='div'> <strong>{profile.name}</strong> </Card.Title>
       </a>
 
       <Card.Text as='div'>
         <p className="my-2">{profile.description}</p>
+      </Card.Text>
+
+      
+      <Card.Text as='div'>
+        <p className="my-2"><strong>Profile Links</strong></p>
+        <a href={`${profile.linkedin}`}>
+          <i className="fa-brands fa-linkedin"></i>
+          {' '} LinkedIn
+        </a>
+        <br/>
+        <a href={`${profile.github}`}>
+          <i className="fa-brands fa-github"></i>
+          {' '} Github
+        </a>
       </Card.Text>
       </Card.Body>
     </Card>
--- frontend/src/teamProfiles.js
+++ frontend/src/teamProfiles.js
@@ -3,31 +3,41 @@ const profiles = [
     _id: '1',
     name: 'Christian Looff',
     image: '/profile-images/Chris2.jpg',
-    description: 'Software Engineer at Team MARCT'
+    description: 'Software Engineer at Team MARCT',
+    linkedin: 'https://www.linkedin.com/in/christian-looff/',
+    github: ''
   },
   {
     _id: '2',
     name: 'Abigail Gerig',
     image: '/profile-images/Abigail.jpg',
-    description: 'Software Engineer at Team MARCT'
+    description: 'Software Engineer at Team MARCT',
+    linkedin: 'https://www.linkedin.com/in/abigail-gerig-49a666239/',
+    github: 'https://github.com/4estgirl'
   },
   {
     _id: '3',
     name: 'Trine Medina',
     image: '/profile-images/Trine.png',
-    description: 'Software Engineer at Team MARCT'
+    description: 'Software Engineer at Team MARCT',
+    linkedin: 'https://www.linkedin.com/in/trinemedina/',
+    github: 'https://github.com/TrineMedina'
   },
   {
     _id: '4',
     name: 'Ruli Warner-Rosen',
     image: '/profile-images/Ruli.png',
-    description: 'Software Engineer at Team MARCT'
+    description: 'Software Engineer at Team MARCT',
+    linkedin: 'https://www.linkedin.com/in/ruliwr',
+    github: 'https://github.com/Ruliwr'
   },
   {
     _id: '5',
     name: 'Matt Dias',
     image: '/profile-images/Matt.png',
-    description: 'Software Engineer at Team MARCT'
+    description: 'Software Engineer at Team MARCT',
+    linkedin: 'https://www.linkedin.com/in/matthew-dias-59850682/',
+    github: 'https://github.com/Schmang13'
   }
 ];
 
